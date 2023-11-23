c100bb6feffa48c090b4fb9e3c83a92229543e51
shihjiajyun <shihjiajyun@gmail.com>
2023-11-23 19:04:53 +0800

    test

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <CardComponent></CardComponent>
   </div>
 </template>
+
+<script>
+import CardComponent from '@/components/CardComponent.vue';
+
+export default {
+  components: {
+    CardComponent,
+  },
+};
+</script>
